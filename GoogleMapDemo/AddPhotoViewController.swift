c452d8a443e223cba6aa395ce34ec0372dfe4279
BoyLeeTW <boy0726@gmail.com>
2017-09-30 16:59:52 +0800

    add camera and album function

--- GoogleMapDemo/AddPhotoViewController.swift
+++ GoogleMapDemo/AddPhotoViewController.swift
@@ -9,28 +9,37 @@
 import UIKit
 import GooglePlaces
 
-import UIKit
-import GooglePlaces
-
-class AddPhotoViewController: UIViewController {
+class AddPhotoViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate, UIGestureRecognizerDelegate {
     
     var resultsViewController: GMSAutocompleteResultsViewController?
     var searchController: UISearchController?
     var resultView: UITextView?
-    
+    let photoImageView  = UIImageView()
+    let imagePicker = UIImagePickerController()
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+
+        photoImageViewSetUp()
+
         resultsViewController = GMSAutocompleteResultsViewController()
         resultsViewController?.delegate = self
         
         searchController = UISearchController(searchResultsController: resultsViewController)
         searchController?.searchResultsUpdater = resultsViewController
         
-        let subView = UIView(frame: CGRect(x: 0, y: 365.0, width: 350.0, height: 45.0))
+        // change position of the search bar
+//        let subView = UIView(frame: CGRect(x: 0, y: 365.0, width: 350.0, height: 45.0))
         
+        let subView = UIView()
+        self.view.addSubview(subView)
+        subView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 0.0).isActive = true
+        subView.trailingAnchor.constraint(equalTo: self.view.trailingAnchor, constant: 0.0).isActive = true
+        subView.topAnchor.constraint(equalTo: self.photoImageView.bottomAnchor, constant: 0.0).isActive = true
+        subView.heightAnchor.constraint(equalToConstant: 45.0).isActive = true
+        subView.translatesAutoresizingMaskIntoConstraints = false
+
         subView.addSubview((searchController?.searchBar)!)
-        view.addSubview(subView)
         searchController?.searchBar.sizeToFit()
         searchController?.hidesNavigationBarDuringPresentation = false
         
@@ -38,6 +47,106 @@ class AddPhotoViewController: UIViewController {
         // this view controller, not one further up the chain.
         definesPresentationContext = true
     }
+
+    func photoImageViewSetUp() {
+
+        self.view.addSubview(photoImageView)
+
+        photoImageView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 0.0).isActive = true
+        photoImageView.trailingAnchor.constraint(equalTo: self.view.trailingAnchor, constant: 0.0).isActive = true
+        photoImageView.topAnchor.constraint(equalTo: self.view.topAnchor, constant: 0.0).isActive = true
+        photoImageView.heightAnchor.constraint(equalToConstant: self.view.frame.height / 2).isActive = true
+        photoImageView.translatesAutoresizingMaskIntoConstraints = false
+
+        photoImageView.backgroundColor = .red
+
+        let tapRecognizer = UITapGestureRecognizer(target: self, action: #selector(handleTapPhotoImageView(sender: )))
+        
+        tapRecognizer.delegate = self
+
+        photoImageView.addGestureRecognizer(tapRecognizer)
+        
+        photoImageView.isUserInteractionEnabled = true
+
+    }
+
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        
+        self.dismiss(animated: true) { () -> Void in
+            
+            if let image = info[UIImagePickerControllerOriginalImage] as? UIImage {
+                
+                self.photoImageView.image = image
+                
+                self.photoImageView.contentMode = .scaleAspectFill
+                
+//                self.journeyImageReminderLabel.isHidden = true
+                
+            } else {
+                
+                print("Something went wrong")
+                
+            }
+            
+        }
+        
+    }
+    
+    @objc func handleTapPhotoImageView(sender: UITapGestureRecognizer) {
+        
+        let photoAlert = UIAlertController(title: "Choose Image", message: nil, preferredStyle: .actionSheet)
+        
+        photoAlert.addAction(UIAlertAction(title: "Take a photo now", style: .default, handler: { _ in
+            
+            self.openCamera()
+            
+        }))
+        
+        photoAlert.addAction(UIAlertAction(title: "Choose from album", style: .default, handler: { _ in
+            
+            self.openAlbum()
+            
+        }))
+        
+        photoAlert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+        
+        self.present(photoAlert, animated: true)
+        
+    }
+
+    func openCamera() {
+        
+        let isCameraExist = UIImagePickerController.isSourceTypeAvailable(.camera)
+        
+        if isCameraExist {
+            
+            imagePicker.delegate = self
+            
+            imagePicker.sourceType = .camera
+            
+            self.present(imagePicker, animated: true)
+            
+        } else {
+            
+            let noCaremaAlert = UIAlertController(title: "Sorry", message: "You don't have camera lol", preferredStyle: .alert)
+            
+            noCaremaAlert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+            
+            self.present(noCaremaAlert, animated: true)
+        }
+        
+    }
+    
+    func openAlbum() {
+        
+        imagePicker.delegate = self
+        
+        imagePicker.sourceType = .photoLibrary
+        
+        self.present(imagePicker, animated: true)
+        
+    }
+
 }
 
 // Handle the user's selection.
@@ -66,31 +175,3 @@ extension AddPhotoViewController: GMSAutocompleteResultsViewControllerDelegate {
         UIApplication.shared.isNetworkActivityIndicatorVisible = false
     }
 }
-
-
-//class AddPhotoViewController: UIViewController {
-//
-//    override func viewDidLoad() {
-//        super.viewDidLoad()
-//
-//        // Do any additional setup after loading the view.
-//    }
-//
-//    override func didReceiveMemoryWarning() {
-//        super.didReceiveMemoryWarning()
-//        // Dispose of any resources that can be recreated.
-//    }
-//
-//
-//    /*
-//    // MARK: - Navigation
-//
-//    // In a storyboard-based application, you will often want to do a little preparation before navigation
-//    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-//        // Get the new view controller using segue.destinationViewController.
-//        // Pass the selected object to the new view controller.
-//    }
-//    */
-//
-//}
-
